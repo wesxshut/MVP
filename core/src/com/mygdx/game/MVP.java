20255872cce2f74714637091e0d1f351e501dbae
wesley <shut.wn97@gmail.com>
2017-06-23 21:00:13 -0300

    Tentativa da exportação e conversão do blender (.fbx) para .G3DB

--- core/src/com/mygdx/game/MVP.java
+++ core/src/com/mygdx/game/MVP.java
@@ -1,33 +1,67 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
+import com.badlogic.gdx.graphics.g3d.utils.AnimationController;
+import com.badlogic.gdx.utils.UBJsonReader;
 
 public class MVP extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private PerspectiveCamera camera;
+    private ModelBatch modelBatch;
+    private Model model;
+    private ModelInstance modelInstance;
+    private Environment environment;
+    private AnimationController controller;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        camera = new PerspectiveCamera(75, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(0f, 100f, 100f);
+        camera.lookAt(0f, 100f, 0f);
+        camera.near = 0.1f;
+        camera.far = 300.0f;
+
+        modelBatch = new ModelBatch();
+        UBJsonReader jsonReader = new UBJsonReader();
+        G3dModelLoader modelLoader = new G3dModelLoader(jsonReader);
+        model = modelLoader.loadModel(Gdx.files.getFileHandle("Robot.g3db", Files.FileType.Internal));
+
+        modelInstance = new ModelInstance(model);
+        modelInstance.transform.translate(0, 0, -2);
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.8f, 0.8f, 1.0f));
+
+        controller = new AnimationController(modelInstance);
+        controller.setAnimation("Plane|Antebraço.L.001Action", -1); //Verificar
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        camera.update();
+        controller.update(Gdx.graphics.getDeltaTime());
+
+        modelBatch.begin(camera);
+        modelBatch.render(modelInstance, environment);
+        modelBatch.end();
+    }
+
+    @Override
+    public void dispose() {
+        modelBatch.dispose();
+        model.dispose();
+
+    }
 }
